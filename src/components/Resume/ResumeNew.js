f0f746fb7bb9736de9a2b9195c82b796831e7d4c
Jose Ueslei De Moraes Ferreira <joseueslei27@gmail.com>
2023-04-05 20:08:27 -0300

    756

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/curriculo.odt";
+import pdf from "../../Assets/../Assets/oi.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-   "https://github.com/uesleiMF/Portifolioues/master/src/Assets/curriculo.odt";
+   "https://github.com/uesleiMF/PortifolioUeslei/blob/main/src/Assets/oi.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
