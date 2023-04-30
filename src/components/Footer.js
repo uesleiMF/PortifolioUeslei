796803c1974c82a8e5f065d2a10d6a8080c189ae
Jose Ueslei De Moraes Ferreira <joseueslei27@gmail.com>
2023-04-29 21:08:59 -0300

    ihl

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Projetado e Desenvolvido por Jose Ueslei</h3>
+          <h3>Ueslei</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} JUMF</h3>
